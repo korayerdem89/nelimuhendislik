b0c335ea97aecebe49620b8580ae13cc10b5459c
korayerdem89 <korayerdem89@gmail.com>
2026-02-13 17:55:31 +0300

    scrollontop is ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
@@ -9,10 +10,21 @@ import Restoration from './pages/Restoration';
 import Corporate from './pages/Corporate';
 import Contact from './pages/Contact';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-white">
+        <ScrollToTop />
         <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
